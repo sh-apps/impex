be2aec8cf635855a8099e0f4a5ac475eb0648f12
Adrian Hesketh <a-h@users.noreply.github.com>
2024-02-26 13:52:58 +0000

    refactor: fix typo

--- deb/download_debs.sh
+++ deb/download_debs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for x in `cat dependancies.txt`; do apt-cache depends -i $x | awk '/Depends:/ {print $2}' | xargs  apt-get download; apt-get download $x; done
+for x in `cat dependencies.txt`; do apt-cache depends -i $x | awk '/Depends:/ {print $2}' | xargs  apt-get download; apt-get download $x; done
 
 #for x in `ls *.deb`; do curl -u "$USERNAME:$PASSWORD" -H "Content-Type: multipart/form-data" --data-binary "@./$x" "https://${NEXUS_HOST}:${NEXUS_PORT}/repository/hosted-apt/"; done
